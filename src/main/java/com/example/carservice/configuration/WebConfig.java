f04af0b0f9cad08c7d2f32bd3292c4d2e5eb6ccb
Achoual Othmane <othmane.achoual@4d.com>
2018-10-05 17:23:02 +0100

    api supports movie deletion

--- src/main/java/com/example/carservice/configuration/WebConfig.java
+++ src/main/java/com/example/carservice/configuration/WebConfig.java
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
             .addMapping("/**")
+            .allowedMethods("*")
             .allowedOrigins("http://localhost:3000")
             .allowedHeaders("*")
             .allowCredentials(true);
---
+++ src/main/java/com/example/carservice/controllers/MovieAdminController.java
@@ -0,0 +1,30 @@
+package com.example.carservice.controllers;
+
+import com.example.carservice.entities.Movie;
+import com.example.carservice.repositories.MovieRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin/movie")
+public class MovieAdminController {
+
+    @Autowired
+    private MovieRepository repository;
+
+    @PostMapping("")
+    public Movie add(@RequestBody Movie movie) {
+        return this.repository.save(movie);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        this.repository.deleteById(id);
+    }
+}
