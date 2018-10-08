11132c2b23cfcc5727a0d54f496d8ac8ceaa070b
Achoual Othmane <othmane.achoual@4d.com>
2018-10-08 17:29:15 +0100

    modifies deserializer class for release date

--- src/main/java/com/example/carservice/json/MyDateDeserializer.java
+++ src/main/java/com/example/carservice/json/MyDateDeserializer.java
@@ -8,21 +8,11 @@ import java.util.Date;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
-// import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 
-public class MyDateDeserializer extends StdDeserializer<Date> {
+public class MyDateDeserializer extends JsonDeserializer<Date> {
 
     private static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-
-    public MyDateDeserializer() {
-        this(null);
-    }
-
-    public MyDateDeserializer(Class<?> vc) {
-        super(vc);
-    }
 
 	@Override
     public Date deserialize(JsonParser p, DeserializationContext ctxt)
