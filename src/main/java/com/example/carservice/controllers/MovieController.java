7a4f34753cc5d0272de984c70609b209718508d2
Achoual Othmane <othmane.achoual@4d.com>
2018-11-02 16:33:57 +0100

    paginates returned list of movies

--- src/main/java/com/example/carservice/controllers/MovieController.java
+++ src/main/java/com/example/carservice/controllers/MovieController.java
@@ -6,8 +6,11 @@ import com.example.carservice.entities.Movie;
 import com.example.carservice.repositories.MovieRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,8 +21,10 @@ public class MovieController {
     private MovieRepository repository;
 
     @GetMapping("")
-    public List<Movie> all() {
-        return this.repository.findAll();
+    public Page<Movie> all(@RequestParam("p") int page) {
+        System.out.println(page);
+        return this.repository.findAll(PageRequest.of(page, 2));
+        // return this.repository.findAll();
     }
 
     @GetMapping("/top")
