59df40d25d8f86857c6bad56daf02863957d020c
Othmane Achoual <othmane.ac@gmail.com>
2018-11-11 01:05:06 +0100

    Adds simple test

--- src/test/java/com/example/carservice/CarServiceApplicationTests.java
+++ src/test/java/com/example/carservice/CarServiceApplicationTests.java
@@ -1,5 +1,9 @@
 package com.example.carservice;
 
+import static org.junit.Assert.assertEquals;
+
+import com.example.carservice.entities.Movie;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +15,13 @@ public class CarServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
-	}
+  }
+  
+  @Test
+  public void movieTest() {
+    Movie movie = new Movie("Foo");
+
+    assertEquals("Movie title not set correctly through constructor", "Foo", movie.getTitle());
+  }
 
 }
